42be20c16e09f12b4eda0f83fe92b657657db076
rodio <rodionmarkelov01229@gmail.com>
2024-05-19 21:05:20 +0400

    Добавьте файлы проекта.

---
+++ graph_aisd_lab/graph_aisd_lab.cpp
@@ -0,0 +1,12 @@
+﻿// graph_aisd_lab.cpp: определяет точку входа для приложения.
+//
+
+#include "graph_aisd_lab.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
