2e47be639a1a8add91e463a8927ea35b413977db
rodio <rodionmarkelov01229@gmail.com>
2024-05-21 10:14:46 +0400

    class without walk+short_path

--- graph_aisd_lab/graph_aisd_lab.cpp
+++ graph_aisd_lab/graph_aisd_lab.cpp
@@ -7,6 +7,54 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	Graph<int, double> graph1;
+	graph1.add_vertex(1);
+	graph1.add_vertex(2);
+	graph1.add_vertex(3);
+	graph1.add_vertex(4);
+	graph1.add_edge(1, 3, 7.5);
+	graph1.add_edge(1, 1, 1.0);
+	graph1.add_edge(1, 2, 2.7);
+	graph1.add_edge(2, 3, 1.45);
+	graph1.add_edge(3, 4, 17.25);
+	graph1.add_edge(4, 2, 3.89);
+
+	cout << graph1;
+
+	cout << "Test has_vertex" << endl;
+	cout << graph1.has_vertex(1) << graph1.has_vertex(4) << graph1.has_vertex(5) << endl;
+
+	cout << "Test remove_vertex" << endl;
+	cout << graph1.remove_vertex(2) << ' ' << graph1.remove_vertex(2) << ' ' << graph1.remove_vertex(5) << endl;
+	cout << graph1;
+
+	cout << "Test vertices()" << endl;
+	vector<int> vec1 = graph1.vertices();
+	for (auto i : vec1) {
+		cout << i << ' ';
+	}
+	cout << endl;
+	
+	cout << "Test has_edge(from, to)" << endl;
+	cout << graph1.has_edge(1, 1) << ' ' << graph1.has_edge(1, 2) << ' ' << graph1.has_edge(1, 3) << endl;
+
+	cout << "Test has_edge(from, edge)" << endl;
+	cout << graph1.has_edge(1, Graph<int, double>::Edge(3, 7.5)) << ' ' << graph1.has_edge(1, Graph<int, double>::Edge(2, 7.5)) << ' ' << graph1.has_edge(1, Graph<int, double>::Edge(1, 3.75)) << endl;
+
+	cout << "Test edges()" << endl;
+	vector<Graph<int, double>::Edge> edges = graph1.edges(1);
+	for (auto i : edges) {
+		cout << i;
+	}
+	cout << endl;
+
+	cout << "Test remove(from, to)" << endl;
+	cout << graph1.remove_edge(1, 1) << ' ' << graph1.remove_edge(1, 2) << ' ' << graph1.remove_edge(1, 3) << endl;
+	cout << graph1;
+
+	cout << "Test remove(from, e)" << endl;
+	cout << graph1.remove_edge(1, Graph<int, double>::Edge(2, 3.75)) << ' ' << graph1.remove_edge(2, Graph<int, double>::Edge(2, 1.45)) << ' ' << graph1.remove_edge(4, Graph<int, double>::Edge(2, 3.89)) << endl;
+	cout << graph1; 
+
 	return 0;
 }
--- graph_aisd_lab/graph_aisd_lab.h
+++ graph_aisd_lab/graph_aisd_lab.h
@@ -1,8 +1,172 @@
-﻿// graph_aisd_lab.h : включаемый файл для стандартных системных включаемых файлов
-// или включаемые файлы для конкретного проекта.
-
-#pragma once
+﻿#pragma once
 
 #include <iostream>
+#include <memory>
+#include <algorithm>
+#include <vector>
+#include <list>
+#include <iterator>
+#include <map>
 
-// TODO: установите здесь ссылки на дополнительные заголовки, требующиеся для программы.
+using namespace std;
+
+template<typename Vertex, typename Distance = double>
+class Graph {
+public:
+	struct Edge {
+		Vertex _to_id;
+		Distance _distance;
+		Edge(Vertex to_id, Distance distance) : _to_id(to_id), _distance(distance) {};
+		bool operator==(const Edge& other) const {
+			return (_to_id == other._to_id && _distance == other._distance);
+		}
+		friend std::ostream& operator<<(std::ostream& stream, const Edge& other) {
+			stream << '(' << other._to_id << ' ' << other._distance << ") ";
+			return stream;
+		}
+	};
+
+private:
+	map<Vertex, list<Edge>> _graph;
+
+public:
+
+	Graph() = default;
+	~Graph() = default;
+
+	//проверка-добавление-удаление вершин
+	bool has_vertex(const Vertex& v) const {
+		if (_graph.size() == 0) return false;
+		else return _graph.contains(v);
+	}
+
+	void add_vertex(const Vertex& v) {
+		if (has_vertex(v)) {
+			cout << "Vertex already in graph." << endl;
+			return;
+		}
+		else {
+			_graph.insert({ v, list<Edge>() });
+		}
+	}
+
+	bool remove_vertex(const Vertex& v) {
+		if (!has_vertex(v)) return false;
+		for (auto i = _graph.begin(); i != _graph.end(); ++i) {
+			if (has_edge(i->first, v)) {
+				remove_edge(i->first, v);
+			}
+		}
+		return _graph.erase(v);
+	}
+
+	std::vector<Vertex> vertices() const {
+		vector<Vertex> vert;
+		if (_graph.size() == 0) return vert;
+		else {
+			for (auto i : _graph) {
+				vert.push_back(i.first);
+			}
+			return vert;
+		}
+	}
+
+	//проверка-добавление-удаление ребер
+	void add_edge(const Vertex& from, const Vertex& to,
+		const Distance& d) {
+		if (has_vertex(from) && has_vertex(to)) {
+			_graph[from].push_back(Edge(to, d));
+		}
+		else {
+			cout << "One of vertexes not exist." << endl;
+			return;
+		}
+	}
+
+
+	bool remove_edge(const Vertex& from, const Vertex& to) {
+		if (has_edge(from, to)) {
+			for (auto i = _graph.at(from).begin(); i != _graph.at(from).end(); ++i) {
+				if ((*i)._to_id == to) {
+					_graph.at(from).erase(i);
+					return true;
+				}
+			}
+		}
+		else return false;
+	}
+
+	bool remove_edge(const Vertex& from, const Edge& e) {//c учетом расстояния
+		if (has_edge(from, e)) {
+			for (auto i = _graph.at(from).begin(); i != _graph.at(from).end(); ++i) {
+				if ((*i) == e) {
+					_graph.at(from).erase(i);
+					return true;
+				}
+			}
+		}
+		else return false;
+	}
+
+	bool has_edge(const Vertex& from, const Vertex& to) const {
+		if (has_vertex(from) && has_vertex(to)) {
+			for (auto i = _graph.at(from).begin(); i != _graph.at(from).end(); ++i) {
+				if ((*i)._to_id == to) {
+					return true;
+				}
+			}
+			return false;
+		}
+		else return false;
+	}
+
+	bool has_edge(const Vertex& from, const Edge& e) const { //c учетом расстояния в Edge
+		if (has_vertex(from) && has_vertex(e._to_id)) {
+			for (auto i = _graph.at(from).begin(); i != _graph.at(from).end(); ++i) {
+				if ((*i) == e) {
+					return true;
+				}
+			}
+			return false;
+		}
+		else return false;
+	}
+
+	//получение всех ребер, выходящих из вершины
+	std::vector<Edge> edges(const Vertex& vertex) {
+		vector<Edge> edges;
+		if (!has_vertex(vertex)) return edges;
+		for (auto i = _graph.at(vertex).begin(); i != _graph.at(vertex).end(); ++i) {
+			edges.push_back((*i));
+		}
+		return edges;
+	}
+
+	size_t order() const { //порядок
+		return _graph.size();
+	}
+	size_t degree(const Vertex& v) const { //степень вершины
+		if (!has_vertex(v)) throw runtime_error("Vertex is not exist.");
+		else {
+			return _graph[v].size();
+		}
+	}
+
+	friend std::ostream& operator<<(std::ostream& stream, const Graph& graph) {
+		for (auto &[vertex, edges] : graph._graph) {
+			stream << "Vertex(" << vertex << "): ";
+			for (auto j : edges) {
+				cout << j;
+			}
+			stream << endl;
+		}
+		return stream;
+	}
+
+	//поиск кратчайшего пути
+	std::vector<Edge> shortest_path(const Vertex& from,
+		const Vertex& to) const;
+	//обход
+	std::vector<Vertex>  walk(const Vertex& start_vertex) const;
+};
+
