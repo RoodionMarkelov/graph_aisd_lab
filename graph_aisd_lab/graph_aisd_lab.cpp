e5fb5bfca5216cddb844eb0898f679dbb7a020be
rodio <rodionmarkelov01229@gmail.com>
2024-05-27 21:56:52 +0400

    class final

--- graph_aisd_lab/graph_aisd_lab.cpp
+++ graph_aisd_lab/graph_aisd_lab.cpp
@@ -7,54 +7,118 @@ using namespace std;
 
 int main()
 {
-	Graph<int, double> graph1;
-	graph1.add_vertex(1);
-	graph1.add_vertex(2);
-	graph1.add_vertex(3);
-	graph1.add_vertex(4);
-	graph1.add_edge(1, 3, 7.5);
-	graph1.add_edge(1, 1, 1.0);
-	graph1.add_edge(1, 2, 2.7);
-	graph1.add_edge(2, 3, 1.45);
-	graph1.add_edge(3, 4, 17.25);
-	graph1.add_edge(4, 2, 3.89);
+	//Graph<int, double> graph1;
+	//graph1.add_vertex(1);
+	//graph1.add_vertex(2);
+	//graph1.add_vertex(3);
+	//graph1.add_vertex(4);
+	//graph1.add_edge(1, 3, 7.5);
+	//graph1.add_edge(1, 1, 1.0);
+	//graph1.add_edge(1, 2, 2.7);
+	//graph1.add_edge(2, 3, 1.45);
+	//graph1.add_edge(3, 4, 17.25);
+	//graph1.add_edge(4, 2, 3.89);
 
-	cout << graph1;
+	//cout << graph1;
 
-	cout << "Test has_vertex" << endl;
-	cout << graph1.has_vertex(1) << graph1.has_vertex(4) << graph1.has_vertex(5) << endl;
+	//cout << "Test has_vertex" << endl;
+	//cout << graph1.has_vertex(1) << graph1.has_vertex(4) << graph1.has_vertex(5) << endl;
 
-	cout << "Test remove_vertex" << endl;
-	cout << graph1.remove_vertex(2) << ' ' << graph1.remove_vertex(2) << ' ' << graph1.remove_vertex(5) << endl;
-	cout << graph1;
+	//cout << "Test remove_vertex" << endl;
+	//cout << graph1.remove_vertex(2) << ' ' << graph1.remove_vertex(2) << ' ' << graph1.remove_vertex(5) << endl;
+	//cout << graph1;
 
-	cout << "Test vertices()" << endl;
-	vector<int> vec1 = graph1.vertices();
-	for (auto i : vec1) {
+	//cout << "Test vertices()" << endl;
+	//vector<int> vec1 = graph1.vertices();
+	//for (auto i : vec1) {
+	//	cout << i << ' ';
+	//}
+	//cout << endl;
+	//
+	//cout << "Test has_edge(from, to)" << endl;
+	//cout << graph1.has_edge(1, 1) << ' ' << graph1.has_edge(1, 2) << ' ' << graph1.has_edge(1, 3) << endl;
+
+	//cout << "Test has_edge(from, edge)" << endl;
+	//cout << graph1.has_edge(1, Graph<int, double>::Edge(3, 7.5)) << ' ' << graph1.has_edge(1, Graph<int, double>::Edge(2, 7.5)) << ' ' << graph1.has_edge(1, Graph<int, double>::Edge(1, 3.75)) << endl;
+
+	//cout << "Test edges()" << endl;
+	//vector<Graph<int, double>::Edge> edges = graph1.edges(1);
+	//for (auto i : edges) {
+	//	cout << i;
+	//}
+	//cout << endl;
+
+	//cout << "Test remove(from, to)" << endl;
+	//cout << graph1.remove_edge(1, 1) << ' ' << graph1.remove_edge(1, 2) << ' ' << graph1.remove_edge(1, 3) << endl;
+	//cout << graph1;
+
+	//cout << "Test remove(from, e)" << endl;
+	//cout << graph1.remove_edge(1, Graph<int, double>::Edge(2, 3.75)) << ' ' << graph1.remove_edge(2, Graph<int, double>::Edge(2, 1.45)) << ' ' << graph1.remove_edge(4, Graph<int, double>::Edge(2, 3.89)) << endl;
+	//cout << graph1 << endl;
+
+	cout << "Test walk(start vertex)" << endl;
+	Graph<int, double> graph2;
+	graph2.add_vertex(1);
+	graph2.add_vertex(2);
+	graph2.add_vertex(3);
+	graph2.add_vertex(4);
+	graph2.add_edge(1, 3, 7.5);
+	graph2.add_edge(1, 1, 1.0);
+	graph2.add_edge(1, 2, 2.7);
+	graph2.add_edge(2, 3, 1.45);
+	graph2.add_edge(3, 4, 17.25);
+	graph2.add_edge(4, 2, 3.89);
+
+	vector<Graph<int, double>::Edge> walked = graph2.walk(1);
+	for (auto i : walked) {
 		cout << i << ' ';
 	}
-	cout << endl;
-	
-	cout << "Test has_edge(from, to)" << endl;
-	cout << graph1.has_edge(1, 1) << ' ' << graph1.has_edge(1, 2) << ' ' << graph1.has_edge(1, 3) << endl;
+	cout << endl << endl;
 
-	cout << "Test has_edge(from, edge)" << endl;
-	cout << graph1.has_edge(1, Graph<int, double>::Edge(3, 7.5)) << ' ' << graph1.has_edge(1, Graph<int, double>::Edge(2, 7.5)) << ' ' << graph1.has_edge(1, Graph<int, double>::Edge(1, 3.75)) << endl;
-
-	cout << "Test edges()" << endl;
-	vector<Graph<int, double>::Edge> edges = graph1.edges(1);
-	for (auto i : edges) {
-		cout << i;
+	cout << "Test shortest_path" << endl;
+	vector<Graph<int, double>::Edge> shortest1 = graph2.shortest_path(1, 4);
+	if (shortest1.size() != 0) {
+		for (auto i : shortest1) {
+			cout << i << ' ';
+		}
 	}
-	cout << endl;
+	cout << endl << endl;
 
-	cout << "Test remove(from, to)" << endl;
-	cout << graph1.remove_edge(1, 1) << ' ' << graph1.remove_edge(1, 2) << ' ' << graph1.remove_edge(1, 3) << endl;
-	cout << graph1;
+	vector<Graph<int, double>::Edge> shortest2 = graph2.shortest_path(2, 4);
+	if (shortest2.size() != 0) {
+		for (auto i : shortest2) {
+			cout << i << ' ';
+		}
+	}
+	cout << endl << endl;
 
-	cout << "Test remove(from, e)" << endl;
-	cout << graph1.remove_edge(1, Graph<int, double>::Edge(2, 3.75)) << ' ' << graph1.remove_edge(2, Graph<int, double>::Edge(2, 1.45)) << ' ' << graph1.remove_edge(4, Graph<int, double>::Edge(2, 3.89)) << endl;
-	cout << graph1; 
+	vector<Graph<int, double>::Edge> shortest3 = graph2.shortest_path(4, 2);
+	if (shortest3.size() != 0) {
+		for (auto i : shortest3) {
+			cout << i << ' ';
+		}
+	}
+	cout << endl << endl;
+
+	Graph<int, double> graph3;
+	graph3.add_vertex(1);
+	graph3.add_vertex(2);
+	graph3.add_vertex(3);
+	graph3.add_vertex(4);
+	graph3.add_edge(1, 3, -7.5);
+	graph3.add_edge(1, 1, -1.0);
+	graph3.add_edge(1, 2, -2.7);
+	graph3.add_edge(2, 3, -1.45);
+	graph3.add_edge(3, 4, -17.25);
+	graph3.add_edge(4, 2, -3.89);
+
+	vector<Graph<int, double>::Edge> shortest4 = graph3.shortest_path(1, 4);
+	if (shortest4.size() != 0) {
+		for (auto i : shortest4) {
+			cout << i << ' ';
+		}
+	}
+	cout << endl << endl;
 
 	return 0;
 }
--- graph_aisd_lab/graph_aisd_lab.h
+++ graph_aisd_lab/graph_aisd_lab.h
@@ -1,12 +1,15 @@
 ﻿#pragma once
 
 #include <iostream>
-#include <memory>
 #include <algorithm>
 #include <vector>
 #include <list>
 #include <iterator>
 #include <map>
+#include <unordered_map>
+#include <queue>
+#include <limits>
+
 
 using namespace std;
 
@@ -16,13 +19,31 @@ public:
 	struct Edge {
 		Vertex _to_id;
 		Distance _distance;
-		Edge(Vertex to_id, Distance distance) : _to_id(to_id), _distance(distance) {};
+		Edge(const Vertex& to_id,const Distance& distance) : _to_id(to_id), _distance(distance) {};
 		bool operator==(const Edge& other) const {
 			return (_to_id == other._to_id && _distance == other._distance);
 		}
 		friend std::ostream& operator<<(std::ostream& stream, const Edge& other) {
-			stream << '(' << other._to_id << ' ' << other._distance << ") ";
+			stream << '(' << other._to_id << ": " << other._distance << ") ";
 			return stream;
+		}
+	};
+
+	struct Walk {
+		string _color;
+		Vertex _prev;
+		Distance _distance;
+		Walk() {
+			_color = "white";
+			_distance = -1;
+		}
+	};
+
+	struct Shortest_ways {
+		Distance _dist;
+		Vertex _prev;
+		Shortest_ways() {
+			_dist = numeric_limits<Distance>::max();
 		}
 	};
 
@@ -164,9 +185,96 @@ public:
 	}
 
 	//поиск кратчайшего пути
-	std::vector<Edge> shortest_path(const Vertex& from,
-		const Vertex& to) const;
+	unordered_map<Vertex, Shortest_ways> Bellman_Ford_algorithm(const Vertex& from, const Vertex& to) {
+		unordered_map<Vertex, Shortest_ways> shortest_ways;
+		vector<Vertex> vert = vertices();
+		for (auto i : vert) {
+			shortest_ways[i] = Shortest_ways();
+		}
+
+		shortest_ways[from]._dist = 0;
+		vector<Edge> _edges = edges(from);
+		for (auto edge : _edges) {
+			if ((shortest_ways[from]._dist != numeric_limits<Distance>::max()) && shortest_ways[edge._to_id]._dist > shortest_ways[from]._dist + edge._distance) {
+				shortest_ways[edge._to_id]._dist = shortest_ways[from]._dist + edge._distance;
+				shortest_ways[edge._to_id]._prev = from;
+			}
+		}
+
+		for (int i = 0; i < vert.size(); ++i) {
+			vector<Edge> _edges = edges(i);
+			for (auto edge : _edges) {
+				if ((shortest_ways[i]._dist != numeric_limits<Distance>::max()) && shortest_ways[edge._to_id]._dist > shortest_ways[i]._dist + edge._distance) {
+					shortest_ways[edge._to_id]._dist = shortest_ways[i]._dist + edge._distance;
+					shortest_ways[edge._to_id]._prev = i;
+				}
+			}
+		}
+
+		for (auto& [vertex, edges] : _graph) {
+			for (auto j : edges) {
+				if ((shortest_ways[vertex]._dist != numeric_limits<Distance>::max()) && shortest_ways[j._to_id]._dist > shortest_ways[vertex]._dist + j._distance) {
+					shortest_ways.clear();
+					return shortest_ways;
+				}
+			}
+		}
+		return shortest_ways;
+	}
+
+	std::vector<Edge> shortest_path(const Vertex& from, const Vertex& to) {
+		if (!has_vertex(from) || !has_vertex(from)) throw runtime_error("Vertex is not found.");
+
+		vector<Edge> result_shortest_ways;
+
+		unordered_map<Vertex, Shortest_ways> result = Bellman_Ford_algorithm(from, to);
+
+		if (result.empty()) {
+			cout << "Shortest_way is not exsist" << endl;
+			return result_shortest_ways;
+		}
+
+		vector<Vertex> vert = vertices();
+		for (auto v : vert) {
+			if (result[v]._dist != numeric_limits<Distance>::max()) {
+				result_shortest_ways.push_back(Edge(v, result[v]._dist));
+			}
+		}
+		return result_shortest_ways;
+	}
+
 	//обход
-	std::vector<Vertex>  walk(const Vertex& start_vertex) const;
+	std::vector<Edge>  walk(const Vertex& start_vertex) const {
+		if (!has_vertex(start_vertex)) throw runtime_error("Vertex is not found.");
+		vector<Edge> result_of_walking;
+
+		unordered_map<Vertex, Walk> walked;
+		vector<Vertex> vert = vertices();
+		for (auto i : vert) {
+			walked[i] = Walk();
+		}
+
+		queue<Vertex> v_queue;
+		v_queue.push(start_vertex);
+		walked[start_vertex]._color = "grey";
+		walked[start_vertex]._distance = 0;
+		result_of_walking.push_back(Edge(start_vertex, 0));
+		while (!v_queue.empty()) {
+			Vertex u = v_queue.front();
+			v_queue.pop();
+			for (auto edge : _graph.at(u)) {
+				Vertex vert = edge._to_id;
+				if (walked[vert]._color == "white") {
+					walked[vert]._color = "grey";
+					walked[vert]._prev = u;
+					walked[vert]._distance = walked[u]._distance + 1;
+					result_of_walking.push_back(Edge(vert, walked[vert]._distance));
+					v_queue.push(vert);
+				}
+			}
+			walked[u]._color == "black";
+		}
+		return result_of_walking;
+	}
 };
 
